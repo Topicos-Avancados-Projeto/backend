49421ecbe58518bf0ec259433ad82ba5b4ae54f6
MariaVasconcelosIFPE <mgsv@discente.ifpe.edu.br>
2023-10-04 20:40:38 -0300

    fix: consertando a rota

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -8,16 +8,13 @@ import {
 import { LoginService } from './login.service';
 import { login_post_schema } from './dto/login_post_schema.dto';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { LocalAuthGuard } from './local-auth.guard';
-import { JwtAuthGuard } from './jwt-auth.guard';
 
 @ApiTags('Login')
 @Controller('login')
 export class LoginController {
     constructor(private loginService: LoginService){}
    
-    //@UseGuards(LocalAuthGuard)
-    @Post('login')
+    @Post()
     @ApiResponse({status: 401, description: 'Incorrect CPF or Password!'})
     @ApiResponse({status: 400, description: 'Malformed request. Check the sent data.'})
     @ApiResponse({status: 422, description: 'Validation Problem.'})
@@ -26,8 +23,7 @@ export class LoginController {
     }
 
     @ApiBearerAuth()
-    @UseGuards(JwtAuthGuard)
-    @Get('login')
+    @Get()
     @ApiResponse({status: 401, description: 'User not logged in!'})
     @ApiResponse({status: 403, description: 'Access forbidden for this user.'})
     async loggedUser(@Request() req): Promise <any>{
