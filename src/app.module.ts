35fe7da534b6e58d38156446f36323dc87be35bb
Raul Holanda Lopes <raulzc00@gmail.com>
2023-10-10 11:03:38 -0300

    fix: corrigindo mensagens de erros do patch

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,6 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LoginModule } from './login/login.module';
 import { BrokerClientModule } from './broker-client/broker-client.module';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
-import { CustomExceptionFilter } from './broker-client/filters/custom-exception.filter';
-import { ErrorInterceptor } from './login/exception/ErrorInterceptor';
 
 @Module({
   imports: [
@@ -16,13 +13,9 @@ import { ErrorInterceptor } from './login/exception/ErrorInterceptor';
     MongooseModule.forRoot(process.env.ATLAS_URI),
     UserModule,
     LoginModule,
-    BrokerClientModule
+    BrokerClientModule,
   ],
   controllers: [AppController],
-  providers: [AppService,
-  {
-    provide: APP_INTERCEPTOR,
-    useClass: ErrorInterceptor
-  }],
+  providers: [AppService],
 })
 export class AppModule {}
