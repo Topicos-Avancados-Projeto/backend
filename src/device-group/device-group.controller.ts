fd024a10a209f8901a0060a87a2f23aa303b7a44
Raul Holanda Lopes <raulzc00@gmail.com>
2023-11-30 16:40:07 -0300

    feat: Device Group completamente implementado

--- src/device-group/device-group.controller.ts
+++ src/device-group/device-group.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, HttpCode } from '@nestjs/common';
+import {
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  Controller,
+  HttpStatus,
+  HttpException,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { Role } from 'src/login/enum/roles.enum';
+import { Types } from 'mongoose';
+import { Roles } from 'src/login/decorator/roles.decorator';
+import { JwtAuth } from 'src/login/decorator/jwt.auth.decorator';
+import { DeviceGroup } from './schema/device-group.schema';
 import { DeviceGroupService } from './device-group.service';
 import { DeviceGroupPostSchema } from './dto/devices_group_post_schema.dto';
-import { JwtAuth } from 'src/login/decorator/jwt.auth.decorator';
-import { Roles } from 'src/login/decorator/roles.decorator';
-import { Role } from 'src/login/enum/roles.enum';
 
 @Controller('device_group')
 @JwtAuth()
@@ -15,5 +30,97 @@ export class DeviceGroupController {
   @HttpCode(HttpStatus.OK)
   create(@Body() deviceGroupPostDto: DeviceGroupPostSchema) {
     return this.deviceGroupService.create(deviceGroupPostDto);
+  }
+
+  @Get()
+  @Roles(Role.ADMIN, Role.USER)
+  @HttpCode(HttpStatus.OK)
+  public find() {
+    return this.deviceGroupService.list();
+  }
+
+  @Get(':id')
+  @Roles(Role.ADMIN, Role.USER)
+  async getUser(@Param('id') param: string) {
+    if (Types.ObjectId.isValid(param)) {
+      const deviceGroup = await this.deviceGroupService.findById(param);
+      if (!deviceGroup) {
+        throw new NotFoundException(`User not found.`);
+      }
+      return deviceGroup;
+    } else {
+      const index = parseInt(param, 10);
+      if (isNaN(index) || index < 1) {
+        throw new BadRequestException('Invalid index');
+      }
+      const deviceGroup = await this.deviceGroupService.findByIndex(index);
+      if (!deviceGroup) {
+        throw new NotFoundException(`User not found.`);
+      }
+      return deviceGroup;
+    }
+  }
+
+  @Patch(':id')
+  @Roles(Role.ADMIN, Role.USER)
+  @HttpCode(HttpStatus.OK)
+  public async patch(
+    @Param('id') param: string,
+    @Body() partialUpdate: Partial<DeviceGroup>,
+  ) {
+    if (Object.keys(partialUpdate).length === 0) {
+      throw new HttpException(
+        'No update parameters provided.',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    if (Types.ObjectId.isValid(param)) {
+      const updatedDocument = await this.deviceGroupService.patchById(
+        param,
+        partialUpdate,
+      );
+      if (!updatedDocument) {
+        throw new NotFoundException(`User not found.`);
+      }
+      return updatedDocument;
+    } else {
+      const index = parseInt(param, 10);
+      if (isNaN(index) || index < 1) {
+        throw new BadRequestException('Invalid index');
+      }
+      const updatedDocument = await this.deviceGroupService.patchByIndex(
+        index,
+        partialUpdate,
+      );
+      if (!updatedDocument) {
+        throw new NotFoundException(`User not found.`);
+      }
+      return updatedDocument;
+    }
+  }
+
+  @Delete(':id')
+  @Roles(Role.ADMIN, Role.USER)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async delete(@Param('id') param: string) {
+    if (Types.ObjectId.isValid(param)) {
+      const deletedDocument = await this.deviceGroupService.deleteById(param);
+      if (!deletedDocument) {
+        throw new NotFoundException(`User not found.`);
+      }
+      return deletedDocument;
+    } else {
+      const index = parseInt(param, 10);
+      if (isNaN(index) || index < 1) {
+        throw new BadRequestException('Index inválido');
+      }
+      const deletedDocument = await this.deviceGroupService.deleteByIndex(
+        index,
+      );
+      if (!deletedDocument) {
+        throw new NotFoundException(`User not found.`);
+      }
+      return deletedDocument;
+    }
   }
 }
--- src/device-group/device-group.service.ts
+++ src/device-group/device-group.service.ts
@@ -1,21 +1,21 @@
 import {
-  ConflictException,
   Injectable,
+  NotFoundException,
   UnprocessableEntityException,
 } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { DeviceGroup } from './schema/device-group.schema';
-import { DeviceGroupPostSchema } from './dto/devices_group_post_schema.dto';
+import mongoose, { Model } from 'mongoose';
 import { Device } from 'src/device/schemas/device.schema';
+import { DeviceGroup } from './schema/device-group.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { DeviceGroupPostSchema } from './dto/devices_group_post_schema.dto';
 
 @Injectable()
 export class DeviceGroupService {
   constructor(
     @InjectModel('DeviceGroup')
     private deviceGroupModel: Model<DeviceGroup>,
-    @InjectModel('Device')
-    private deviceModel: Model<Device>,
+    // @InjectModel('Device')
+    // private deviceModel: Model<Device>,
   ) {}
 
   public async isIdUnique(id: any): Promise<boolean> {
@@ -35,9 +35,89 @@ export class DeviceGroupService {
       throw new UnprocessableEntityException('Validation Problem');
     }
     const deviceGroup = await this.deviceGroupModel.create(deviceGroupDto);
-    // if (!(await this.isIdUnique(deviceGroup._id))) {
-    //   throw new ConflictException('ID alread exists!');
-    // }
     return { id: deviceGroup._id, name, devices, description, attributes };
   }
+
+  public async list(): Promise<DeviceGroup[]> {
+    return this.deviceGroupModel.find().sort({ createdAt: -1 }).limit(10).exec();
+  }
+
+  public async findById(id: string): Promise<DeviceGroup> {
+    try {
+      const document = await this.deviceGroupModel.findById(id).exec();
+      if (!document) {
+        throw new NotFoundException(`Device Group not found.`);
+      }
+      return document;
+    } catch (error) {
+      throw new NotFoundException(`Device Group not found.`);
+    }
+  }
+
+  async findByIndex(index: number): Promise<DeviceGroup | null> {
+    return this.deviceGroupModel
+      .findOne()
+      .sort({ createdAt: -1 })
+      .skip(index - 1)
+      .exec();
+  }
+
+  public async patchById(
+    id: string,
+    partialUpdate: Partial<DeviceGroup>,
+  ): Promise<DeviceGroup> {
+    const document = await this.findById(id);
+    if (!document) {
+      throw new NotFoundException(`Device Group not found.`);
+    }
+    Object.assign(document, partialUpdate);
+    const updatedDocument = await document.save();
+    return updatedDocument;
+  }
+
+  async patchByIndex(
+    index: number,
+    partialUpdate: Partial<DeviceGroup>,
+  ): Promise<DeviceGroup | null> {
+    const document = await this.deviceGroupModel
+      .findOne()
+      .sort({ createdAt: -1 })
+      .skip(index - 1)
+      .exec();
+    if (!document) {
+      return null;
+    }
+    Object.assign(document, partialUpdate);
+    try {
+      await document.save();
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        throw new UnprocessableEntityException('Validation Problem.');
+      }
+      throw error;
+    }
+    return document;
+  }
+  
+  public async deleteById(id: string): Promise<DeviceGroup> {
+    const document = await this.findById(id);
+    if (!document) {
+      throw new NotFoundException(`User not found.`);
+    }
+    const deletedDocument = await this.deviceGroupModel.findByIdAndRemove(id).exec();
+    return deletedDocument;
+  }
+  
+    async deleteByIndex(index: number): Promise<DeviceGroup | null> {
+      const document = await this.deviceGroupModel
+        .findOne()
+        .sort({ createdAt: -1 })
+        .skip(index - 1)
+        .exec();
+      if (!document) {
+        return null;
+      }
+      await document.deleteOne();
+      return document;
+    }
 }
